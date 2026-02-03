6cdbc78f3ca377e03f2b4beeeb6e11902dc7d6ae
sknihal11 <24331a42a8@mvgrce.edu.in>
2026-02-03 22:41:20 +0530

    Create firebase.js

---
+++ pong/js/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzomf89xZhgcPsfYXKsYspCt-CiUVzjBA",
+    authDomain: "webarcade-d4c3d.firebaseapp.com",
+    databaseURL: "https://webarcade-d4c3d-default-rtdb.firebaseio.com/",
+    projectId: "webarcade-d4c3d",
+    storageBucket: "webarcade-d4c3d.appspot.com",
+    messagingSenderId: "918209461327",
+    appId: "1:918209461327:web:bd9c1a2431f9b5c66ffe3a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
